bfaf592b4fd32edb78ba2896ac567a921c41237f
xurmeinBackUps <wj.pavocrist@gmail.com>
2019-06-27 15:09:57 -0500

    beep-boop

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,13 +2,12 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcasr 'room_channel', message: render_message(message)
+    ActionCable.server.broadcast 'room_channel', message: render_message(message)
   end
 
   private
 
   def render_message(message)
-    ApplicationController.renderer.render(partial: 'messages/messages', locals: { message: message })
-
+    ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
   end
 end
